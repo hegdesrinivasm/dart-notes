0a0cb9a1433be1bb3409ddb5e46725773178b77a
Srinivas Hegde M <hegdesrinivasm@gmail.com>
2024-06-02 21:41:42 +0530

    File containing variable declarations in dart.

---
+++ var.dart
@@ -0,0 +1,19 @@
+void main() {
+  //Using the var keyword:
+  var name = "Srinivas";
+
+  //By specifying the data type:
+  String name1 = "Srinivas";
+  int x = 69;
+  print(x);
+
+  //You can use the dynamic keyword to declare variables that can change their data type:
+  dynamic name2 = "Srinivas";
+
+  //Constants are declared using the const keyword:
+  const double pi = 3.14;
+  const String name3 = "Hegde";
+
+  //Declaring but not using it
+  String name4;
+}
